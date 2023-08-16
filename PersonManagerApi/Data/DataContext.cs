b47d56761fc9d7bcbc692f6c37e74a4a47a0c406
Broodco <broodcoorens.maxime@gmail.com>
2023-08-16 12:00:47 +0200

    ADD Database configuration ADD Person Model

---
+++ PersonManagerApi/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonManagerApi.Models;
+
+namespace PersonManagerApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
